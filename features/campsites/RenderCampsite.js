30580d0d5327eaea06dcc7fb4a53f1e88d84d4f1
Joshua Macias <jomacias1@gmail.com>
2022-12-26 17:39:53 -0800

    Secure Store

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -14,6 +14,8 @@ const RenderCampsite = (props) => {
 
     const isLeftSwipe = ({ dx }) => dx < -200;
 
+    const isRightSwipe = ({ dx }) => dx > 200;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -44,6 +46,11 @@ const RenderCampsite = (props) => {
                     ],
                     { cancelable: false }
                 )
+
+            }
+
+            else if (isRightSwipe(gestureState)) {
+                props.onShowModal()
 
             }
         }
